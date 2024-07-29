cb95468dcab9211462331e45b9e5a0adc9a0a8eb
Yash <yashaggarwal960@gmail.com>
2024-07-30 04:02:04 +0530

    handle register without api

--- src/lib/action.js
+++ src/lib/action.js
@@ -1,22 +1,53 @@
 "use server";
-
-import { redirect } from "next/navigation";
+import prisma from "@/lib/prisma";
+import bcrypt from "bcryptjs";
 
 export const handleRegisterSubmit = async (formData) => {
-
   try {
-    const api_url = process.env.REGISTER_API_URL;
-    const response = await fetch(api_url, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(formData),
+    const { name, username, email, password, country, contactNumber, address } =
+      await formData;
+
+    // Validate input data (you can use a library like Joi or custom validation)
+    if (
+      !name ||
+      !username ||
+      !email ||
+      !password ||
+      !country ||
+      !contactNumber ||
+      !address
+    ) {
+      return { error: "All fields are required" }, { status: 400 };
+    }
+
+    const existingUser = await prisma.user.findFirst({
+      where: {
+        OR: [{ email }, { username }, { contactNumber }],
+      },
     });
 
-    if (response.ok) {
-      return { status: 'Register Success' };
+    if (existingUser) {
+      return { error: "User already exists" }, { status: 400 };
     }
+
+    const saltIndex = bcrypt.genSaltSync(10);
+    const hashedPassword = await bcrypt.hash(password, saltIndex);
+    await prisma.user.create({
+      data: {
+        name,
+        username,
+        email,
+        password: hashedPassword,
+        country,
+        contactNumber,
+        address,
+        status: true,
+      },
+    });
+
+    return { status: "Register Success" };
   } catch (error) {
-    console.error('Failed to register', error);
-    return { status: 'Register Failed' };
+    console.error("Failed to register", error);
+    return { status: "Register Failed" };
   }
 };
